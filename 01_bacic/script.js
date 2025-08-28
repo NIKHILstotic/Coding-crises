8d50713d98f716b2804360a9239ad28ad39f4a28
Nikhil <139929422+NIKHILstotic@users.noreply.github.com>
2025-08-28 06:19:08 +0000

    i did it meow

--- 01_bacic/script.js
+++ 01_bacic/script.js
@@ -1,10 +1,23 @@
-let a = 10;
-{
-    let a = 25;
-    console.log("inside:", a);
-    
-    
-}
-console.log("outside:", a)
+console.log(typeof "");
 
 
+
+
+// do this !! in concole to chaek if the values is (true) or (false)
+
+// 1 dont use var var is scoped to function only
+
+// 2 let 
+// scoped to block ({}) can be ressinged can not be redeclared hoisted but stays in the  (TDZ) zone
+
+// 3 const-Constant values 
+//scoped to block ({}) cannot be ressinged or redeclared (TDZ)  applies here to
+
+
+
+
+// ? concatenation in javescript + have tow values + can add and it can concatenat
+// "5" +  = 51 and 5 + 1 = 6 
+// if you put "" so it concatenat
+//"vani" + tiwari = "vanitiwari"
+//but - can only  subtract
